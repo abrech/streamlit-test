6d819fb0a7779a827b6bdbba7de0bde5c2dff90c
abrech <adrian.brechtken@gmail.com>
2024-11-01 15:39:04 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import random
+
+a = 5
+st.title("Hello world")
+
+st.write(f"This is my first web {a}pp.")
+
+# st.camera_input("Kamera")
+
+# program reruns on user input
+g = st.number_input(label="hi", step=1)
+rnd = random.randint(1,10)
+st.write(f"Random: {rnd}")
+
+st.write(g)
+
+# session dictionary session_state
+if "rnd" not in st.session_state:
+    st.session_state.rnd = random.randint(1, 100)
+st.write(f"Not random: {st.session_state.rnd}")
---
+++ notes.md
@@ -0,0 +1,4 @@
+## Streamlit stuff
+- stateless -> need to store info in **sessions**, which are stored in cookies
+    - sessions are bound to browser tab (session_state)
+- program reruns on reload and user input (submit without reload)
